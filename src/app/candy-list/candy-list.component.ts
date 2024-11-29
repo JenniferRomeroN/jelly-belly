2fbd403a372edc500351d14418798f7bc78a4283
jenni <jennifer_romero3@hotmail.com>
2024-11-29 16:16:09 -0700

    adding candyList component

---
+++ src/app/candy-list/candy-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candy-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './candy-list.component.html',
+  styleUrl: './candy-list.component.scss'
+})
+export class CandyListComponent {
+
+}
