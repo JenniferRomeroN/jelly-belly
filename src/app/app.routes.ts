1fa61bea1f9a8e02243a01adb0f9bbef772b1d29
jenni <jennifer_romero3@hotmail.com>
2024-12-01 14:54:01 -0700

    adding route customComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Component} from '@angular/core';
+import path from 'path';
+import { CustomComponent } from './custom/custom.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: CustomComponent},
+];
