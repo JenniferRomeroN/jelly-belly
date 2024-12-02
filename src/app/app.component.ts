ac1b8b1f75e38b6b0a38616bb179846a07291fca
jenni <jennifer_romero3@hotmail.com>
2024-12-01 22:02:15 -0700

    change routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CustomComponent } from './custom/custom.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CustomComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CustomComponent } from './custom/custom.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+  { path: '', component: HomeComponent }, // Ruta para Home
+  { path: 'custom', component: CustomComponent }, // Ruta para Custom
 ];
